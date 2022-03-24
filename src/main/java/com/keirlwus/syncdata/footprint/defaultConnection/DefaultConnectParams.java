e12829e4124ed9bd21534fbe45d0ca6565952fd7
WuShanMuYuu <1064140997@qq.com>
2022-03-24 16:51:34 +0800

    no update

---
+++ src/main/java/com/keirlwus/syncdata/footprint/defaultConnection/DefaultConnectParams.java
@@ -0,0 +1,19 @@
+package com.keirlwus.syncdata.footprint.defaultConnection;
+
+/**
+ * Created by krielwus on 2022-03-24 10:52
+ *
+ * @author krielwus
+ */
+public class DefaultConnectParams {
+
+    //ORACLE连接
+    public static String ORA_TYPE = "ORACLE";
+    public static String ORA_URL = "jdbc:oracle:thin:@//127.0.0.1:1521/tengyi";
+    public static String ORA_USERNAME = "productmanager";
+    public static String ORA_PASSWORD = "root123456";
+    public static String ORA_DRIVERCLASSNAME = "oracle.jdbc.driver.OracleDriver";
+
+
+
+}
